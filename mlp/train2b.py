3b369f0f04f562faf87060acce5ef4dfdba7df1a
Nandi Robijns <Nandi@Nandi-Robijnss-MacBook-Pro.local>
2017-11-27 03:32:06 +0000

    train2b added

---
+++ mlp/train2b.py
@@ -0,0 +1,119 @@
+#re-initialize neural network weights and reset the data providers so you get a properly initialized experiment.
+
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+#plt.style.use('ggplot')
+
+def train_model_and_plot_stats(
+        model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=True):
+    
+    # As well as monitoring the error over training also monitor classification
+    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
+    data_monitors={'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
+
+    # Use the created objects to initialise a new Optimiser instance.
+    optimiser = Optimiser(
+        model, error, learning_rule, train_data, valid_data, data_monitors, notebook=notebook)
+
+    # Run the optimiser for 5 epochs (full passes through the training set)
+    # printing statistics every epoch.
+    stats, keys, run_time = optimiser.train(patience=10, max_num_epochs=num_epochs, stats_interval=stats_interval)
+
+    # # Plot the change in the validation and training set error over training.
+    # fig_1 = plt.figure(figsize=(8, 4))
+    # ax_1 = fig_1.add_subplot(111)
+    # for k in ['error(train)', 'error(valid)']:
+    #     ax_1.plot(np.arange(1, stats.shape[0]) * stats_interval, 
+    #               stats[1:, keys[k]], label=k)
+    # ax_1.legend(loc=0)
+    # ax_1.set_xlabel('Epoch number')
+
+    # # Plot the change in the validation and training set accuracy over training.
+    # fig_2 = plt.figure(figsize=(8, 4))
+    # ax_2 = fig_2.add_subplot(111)
+    # for k in ['acc(train)', 'acc(valid)']:
+    #     ax_2.plot(np.arange(1, stats.shape[0]) * stats_interval, 
+    #               stats[1:, keys[k]], label=k)
+    # ax_2.legend(loc=0)
+    # ax_2.set_xlabel('Epoch number')
+    
+    return stats, keys, #run_time, fig_1, ax_1, fig_2, ax_2
+
+
+
+    # The below code will set up the data providers, random number
+# generator and logger objects needed for training runs. As
+# loading the data from file take a little while you generally
+# will probably not want to reload the data providers on
+# every training run. If you wish to reset their state you
+# should instead use the .reset() method of the data providers.
+import numpy as np
+import logging
+from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider
+
+# Seed a random number generator
+seed = 10102016 
+rng = np.random.RandomState(seed)
+batch_size = 100
+# Set up a logger object to print info about the training run to stdout
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers = [logging.StreamHandler()]
+
+# Create data provider objects for the MNIST data set
+train_data = EMNISTDataProvider('train', batch_size=batch_size, rng=rng)
+valid_data = EMNISTDataProvider('valid', batch_size=batch_size, rng=rng)
+
+
+
+
+# The model set up code below is provided as a starting point.
+# You will probably want to add further code cells for the
+# different experiments you run.
+
+from mlp.layers import AffineLayer, SoftmaxLayer, SigmoidLayer, ReluLayer, LeakyReluLayer, ELULayer, SELULayer
+from mlp.errors import CrossEntropySoftmaxError
+from mlp.models import MultipleLayerModel
+from mlp.initialisers import ConstantInit, GlorotUniformInit
+from mlp.learning_rules import GradientDescentLearningRule
+from mlp.optimisers import Optimiser
+
+#setup hyperparameters
+learning_rate = 0.1
+num_epochs = 200
+stats_interval = 1
+input_dim, output_dim = 784, 47
+
+
+def build_network(hidden_dim, n_layers, activation, incl_prob=None):
+    weights_init = GlorotUniformInit(rng=rng)
+    biases_init = ConstantInit(0.)
+    l = [AffineLayer(input_dim, hidden_dim, weights_init, biases_init), activation]
+    for i in range(n_layers-1):
+        if incl_prob is not None:
+            l += [DropoutLayer(rng=rng, incl_prob=incl_prob) AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init), activation]
+        else:
+            l += [AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init), activation]
+    l += [AffineLayer(hidden_dim, output_dim, weights_init, biases_init)]
+
+    return MultipleLayerModel(l)
+
+
+error = CrossEntropySoftmaxError()
+# Use a basic gradient descent learning rule
+learning_rules = GradientDescentLearningRule(learning_rate=learning_rate), Adam(learning_rate=learning_rate), RMSProp(learning_rate=learning_rate)
+lrule_names = ['sgd', 'adam', 'rms']
+
+#Remember to use notebook=False when you write a script to be run in a terminal
+for i in np.arange(20):
+    seed += i
+    for name, learning_rule in zip(lrule_names, learning_rules):
+        for dout_prob in [0.5, 0.8, None]:
+            for l_rate in [0.1, 0.01, 0.001]:
+                model = build_network(400, 4, ReluLayer(), incl_prob=dout_prob)
+                train_data.reset()
+                valid_data.reset()
+                stats = train_model_and_plot_stats(
+                    model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=False)
+                path = '/afs/inf.ed.ac.uk/user/s17/s1786262/mlpractical/results/' + str(seed) + '_' + 'run' + str(i) + '_' + str(name) + '_' + str(dout_prob) + '_' + str(l_rate)
+                np.save(path, stats)
